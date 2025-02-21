4ecc9fcfcdca47c36d51266b91d61aebb46817ce
pedrohenriquepacheco <tarefaebac23@gmail.com>
2025-02-21 10:58:05 -0300

    commit inicial

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ $('.menu-hamburguer').click(function () {
 })
 
 $('#telefone').mask('(00) 00000-0000');
+$('#cpf').mask('000.000.000-00');
+$('#cep').mask('00000-000');
 
 $('form').validate({
 rules: {
